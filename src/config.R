20e3ff2df638718564004739cd5e96e8c0fe67cd
llanza6 <99312297+llanza6@users.noreply.github.com>
2022-11-12 14:46:15 +0100

    Create config.R

---
+++ src/config.R
@@ -0,0 +1,13 @@
+###################################################################################################
+# Author: xx
+# Project: xx
+# File: config.R
+# Description: configuration file for session inputs
+###################################################################################################
+# Input: 
+# Output: main inputs to be used in GLOBAL.env
+###################################################################################################
+
+# external ip and port for calling api's in front_end
+external_ip = "35.240.13.23"
+port = "8080"
