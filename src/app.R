236e67ff2a9f9e9aa4c5833bce6a768c44740ac0
llanza6 <99312297+llanza6@users.noreply.github.com>
2022-11-27 12:17:41 +0100

    Update app.R

--- src/app.R
+++ src/app.R
@@ -117,7 +117,7 @@ ui <- fluidPage(
                                   title = div(img(src="https://github.com/ZverM7/front_end/blob/main/www/logo.png?raw=true", 
                                               width= "35%"), 
                                               #height= '70px', 
-                                              style="padding-left:5px;padding-top:10px"), 
+                                              style="padding-left:5px;padding-top:10px; padding-right:100px"), 
                                         # picture has to go in www folder
                                   id = "navbar",
                                   selected = "Home",
