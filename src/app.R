cb975317020cd2a9573a72c109bfc64ebe13e4c8
ZverM7 <115363819+ZverM7@users.noreply.github.com>
2022-11-27 12:27:22 +0100

    Update app.R

--- src/app.R
+++ src/app.R
@@ -115,10 +115,9 @@ ui <- fluidPage(
   #navigation tabs: create the tabs and define the theme with css file
                       navbarPage(
                                   title = div(img(src="https://github.com/ZverM7/front_end/blob/main/www/logo.png?raw=true", 
-                                              width= "35%"), 
-                                              align = "right",
+                                              width= "35%"),
                                               #height= '70px', 
-                                              style="padding-left:5px;padding-top:10px"), 
+                                              style="padding-left:5px;padding-top:10px; padding-right:100px"), 
                                         # picture has to go in www folder
                                   id = "navbar",
                                   selected = "Home",
